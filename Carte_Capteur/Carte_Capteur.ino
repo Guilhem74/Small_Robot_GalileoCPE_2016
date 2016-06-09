6491c762ad59fffbc4ccecb77722b4e514b54cc1
Guilhem AZZANO <g.azzano74@gmail.com>
2016-06-09 21:12:14 +0200

    Capteur ultrason avant

--- Carte_Capteur/Carte_Capteur.ino
+++ Carte_Capteur/Carte_Capteur.ino
@@ -4,11 +4,17 @@
 #define BAUDRATE_CAPTEUR 115200
 int seuilAv = 70;
 int seuilAr = 200;
+int seuilArCm = 8;
+long lecture_echo;
+long cm;
 #define PIN_AVANT_GAUCHE A2
 #define PIN_AVANT_DROIT A3
 #define PIN_ARRIERE_GAUCHE A1
 #define PIN_ARRIERE_MILLIEU A0
 #define PIN_ARRIERE_DROIT A4
+#define PIN_ULTRASON_ARRIERE_TRIG 4
+#define PIN_ULTRASON_ARRIERE_ECHO 3
+
 
 int AvD = 0;
 int ArC = 0;
@@ -17,7 +23,9 @@ int ArD = 0;
 int ArG = 0;
 
 void setup() {
-  
+  pinMode(PIN_ULTRASON_ARRIERE_TRIG, OUTPUT);
+  digitalWrite(PIN_ULTRASON_ARRIERE_TRIG, LOW);
+  pinMode(PIN_ULTRASON_ARRIERE_ECHO, INPUT); 
   Serial.begin(BAUDRATE_CAPTEUR);
 }
 
@@ -42,7 +50,11 @@ void loop() {
   ArC = ArC/7;
   ArD = ArD/7;
   ArG = ArG/7;
-  
+  digitalWrite(PIN_ULTRASON_ARRIERE_TRIG, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(PIN_ULTRASON_ARRIERE_TRIG, LOW);
+  lecture_echo = pulseIn(PIN_ULTRASON_ARRIERE_ECHO, HIGH);
+  cm = lecture_echo / 58; 
   if (AvD < seuilAv || AvG < seuilAv)
   {
 
@@ -50,12 +62,20 @@ void loop() {
     Serial.print("\n");
    
   }
-  if (ArD > seuilAr || ArG > seuilAr || ArC > seuilAr)
+  if (cm < seuilArCm)
   {
     Serial.print('S');
     
     Serial.print("\n");
   }
+  /* Desactivation des capteurs infrarouges
+  if (ArD > seuilAr || ArG > seuilAr || ArC > seuilAr)
+  {
+    Serial.print('S');
+     
+    Serial.print("\n");
+  }
+  */
   AvD = 0;
   ArC = 0;  
   AvG = 0;
